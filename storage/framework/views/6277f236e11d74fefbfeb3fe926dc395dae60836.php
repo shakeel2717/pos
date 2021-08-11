464fdce29354bebddddd8ac107967bb9ffa44b18
shakeel2717 <shakeel2717@gmail.com>
2021-08-11 06:56:15 -0700

    Fresh Unzip File

---
+++ storage/framework/views/6277f236e11d74fefbfeb3fe926dc395dae60836.php
@@ -0,0 +1,3 @@
+<h1>Congratulation <?php echo e($name); ?>!</h1>
+<h3>Hope that you will be dedicated and honest with your job</h3>
+<p>Thank you</p><?php /**PATH /home/epos/mpg.e-pos.pk/resources/views/mail/biller_create.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e9a440a4b90eca9aa19a5cb70447fa7d1042cab1.php
@@ -0,0 +1,3 @@
+<h1>Congratulation <?php echo e($name); ?>!</h1>
+<h3>Hope that our deal will be prosperous</h3>
+<p>Thank you</p><?php /**PATH /home/epos/mpg.e-pos.pk/resources/views/mail/supplier_create.blade.php ENDPATH**/ ?>
